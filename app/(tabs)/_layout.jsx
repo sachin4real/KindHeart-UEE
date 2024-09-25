68bb08cf78b351afffd0dba1fcfd32f646624f8c
Sachintha Waduge <isurusachin123@gmail.com>
2024-09-25 15:05:48 +0530

    add Home Page Demo

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
+
 
 export default function TabLayout() {
   return (
    <Tabs screenOptions={{headerShown:false}}>
-    <Tabs.Screen name = 'home'/>
-    <Tabs.Screen name = 'profile'/>
+    <Tabs.Screen name = 'home' 
+    options={{
+      tabBarLabel:'Home',
+      tabBarIcon:({color}) =><Entypo name="home" size={24} color="black" />
+    }}
+    />
+    <Tabs.Screen name = 'profile'
+    options={{
+      tabBarLabel:'Profile',
+      tabBarIcon:({color}) =><Feather name="user" size={24} color="black" />
+    }}
+    />
    </Tabs>
   )
 }
